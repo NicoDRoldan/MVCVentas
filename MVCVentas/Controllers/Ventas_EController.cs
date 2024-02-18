4d23037c640faaa3a52deb7f31c6e9ff28e74460
Nicolas Roldán <nico_2038@hotmail.com>
2024-02-18 13:57:23 -0300

    Se implementan boton de reimpresión de factura en Index y Details de Ventas_EController.

--- MVCVentas/Controllers/Ventas_EController.cs
+++ MVCVentas/Controllers/Ventas_EController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MVCVentas.Data;
 using MVCVentas.Models;
+using Newtonsoft.Json;
 
 namespace MVCVentas.Controllers
 {
@@ -55,7 +56,11 @@ namespace MVCVentas.Controllers
                 .Include(v => v.Modulo)
                 .Include(v => v.Sucursal)
                 .Include(v => v.Usuario)
+                .Include(v => v.Ventas_D)
+                .Include(v => v.Ventas_I)
                 .FirstOrDefaultAsync();
+
+            ViewData["Ventas_E"] = vMVentas_E;
 
             // Traer los detalles de la venta:
             var vMVentas_D = await _context.VMVentas_D
@@ -87,6 +92,57 @@ namespace MVCVentas.Controllers
             }
 
             return View(vMVentas_E);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LlamarAReimpresion(string numVenta, string codComprobante, string codModulo, string numSucursal)
+        {
+            var vMVentas_E = await _context.VMVentas_E
+                .Where(ve => ve.NumVenta == numVenta
+                    && ve.CodComprobante == codComprobante
+                    && ve.CodModulo == codModulo
+                    && ve.NumSucursal == numSucursal
+            )
+                .Include(v => v.Cliente)
+                .Include(v => v.Comprobante)
+                .Include(v => v.FormaPago)
+                .Include(v => v.Modulo)
+                .Include(v => v.Sucursal)
+                .Include(v => v.Usuario)
+                .Include(v => v.Ventas_D)
+                .Include(v => v.Ventas_I)
+                .FirstOrDefaultAsync();
+
+            var vMVentas_D = await _context.VMVentas_D
+                .Where(vd => vd.NumVenta == numVenta
+                    && vd.CodComprobante == codComprobante
+                    && vd.CodModulo == codModulo
+                    && vd.NumSucursal == numSucursal
+                )
+                .Include(vd => vd.Articulo)
+                .ToListAsync();
+
+            var vMVentas_I = await _context.VMVentas_I
+                .Where(vi => vi.NumVenta == numVenta
+                    && vi.CodComprobante == codComprobante
+                    && vi.CodModulo == codModulo
+                    && vi.NumSucursal == numSucursal
+                )
+                .Include(vi => vi.Concepto)
+                .ToListAsync();
+
+            try
+            {
+                VentasController ventasController = new VentasController(_context);
+
+                ventasController.ImprimirReporte(vMVentas_E, vMVentas_D, vMVentas_I);
+
+                return Json(new { success = true, message = "Se realizó la reimpresión! :)" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Error al realizar la reimpresión :(" });
+            }
         }
 
         // GET: Ventas_E/Create
