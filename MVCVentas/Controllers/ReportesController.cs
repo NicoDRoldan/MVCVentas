5001ebcc7b8e30e5df35fb13089f0bc14fc19c04
Nicolas Roldán <nico_2038@hotmail.com>
2024-04-07 02:32:22 -0300

    Se  implementa controlador para reportes. Se implementa metodo para traer todas las ventas por transacciones. Se implementa vista para mostrar todas las ventas por transacciones.

---
+++ MVCVentas/Controllers/ReportesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCVentas.Data;
+
+namespace MVCVentas.Controllers
+{
+    public class ReportesController : Controller
+    {
+        private readonly MVCVentasContext _context;
+
+        public ReportesController(MVCVentasContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ReporteTipoTransaccion()
+        {
+            var reporteContext = _context.VMVentas_TipoTransaccion
+                .Include(v => v.Ventas_E)
+                .Include(v => v.VMTipoTransaccion)
+                .OrderByDescending(ve => ve.Ventas_E.Fecha)
+                .ThenByDescending(ve => ve.Ventas_E.Hora);
+
+            return View(await reporteContext.ToListAsync());
+        }
+    }
+}
