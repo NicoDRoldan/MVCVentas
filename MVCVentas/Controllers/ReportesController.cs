71da2d6cb3847cf65f71d1a8c471c310c7c3e40d
Nicolas Roldán <nico_2038@hotmail.com>
2024-04-07 11:06:24 -0300

    Se implementa reporte de ventas totales por transacción.-

--- MVCVentas/Controllers/ReportesController.cs
+++ MVCVentas/Controllers/ReportesController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MVCVentas.Data;
+using MVCVentas.Models;
 
 namespace MVCVentas.Controllers
 {
+    [Authorize]
     public class ReportesController : Controller
     {
         private readonly MVCVentasContext _context;
@@ -20,13 +23,33 @@ namespace MVCVentas.Controllers
 
         public async Task<IActionResult> ReporteTipoTransaccion()
         {
-            var reporteContext = _context.VMVentas_TipoTransaccion
+            var reportTransactionContext = _context.VMVentas_TipoTransaccion
                 .Include(v => v.Ventas_E)
                 .Include(v => v.VMTipoTransaccion)
                 .OrderByDescending(ve => ve.Ventas_E.Fecha)
                 .ThenByDescending(ve => ve.Ventas_E.Hora);
 
-            return View(await reporteContext.ToListAsync());
+            return View(await reportTransactionContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> ReporteTotalTransacciones()
+        {
+            var totalTransactionContext = await _context.VMVentas_TipoTransaccion
+                .Join(_context.VMTipoTransaccion,
+                    vt => vt.CodTipoTran,
+                    tt => tt.CodTipoTran,
+                    (vt, tt) => new { VentaTran = vt, TipoTran = tt })
+                .GroupBy(vtt => new { vtt.TipoTran.Nombre, vtt.VentaTran.CodTipoTran })
+                .Select(vtt => new VMReporte
+                {
+                    Nombre = vtt.Key.Nombre,
+                    CodTipoTran = vtt.Key.CodTipoTran,
+                    VentasRealizadas = vtt.Count()
+                })
+                .OrderByDescending(vtt => vtt.VentasRealizadas)
+                .ToListAsync();
+                
+            return View(totalTransactionContext);
         }
     }
 }
---
+++ MVCVentas/Models/VMReporte.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCVentas.Models
+{
+    public class VMReporte
+    {
+        public string Nombre { get; set; }
+
+        public string CodTipoTran { get; set; }
+
+        public int VentasRealizadas { get; set; }
+    }
+}
