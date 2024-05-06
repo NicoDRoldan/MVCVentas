897b361e3d0fb8faf048012d689abdbcf6b379d9
Nicolas Roldán <nico_2038@hotmail.com>
2024-05-05 23:33:12 -0300

    Implementación Cupones: - Se agrega lógica para que, al obtener un cupón, enviar desde el servidor, un objeto json con los artículos correspondientes a un cupón. - Se implementan los modelos correspondientes para poder realizar a cabo la acción.

---
+++ MVCVentas/Models/VMCDetallel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCVentas.Models
+{
+    public class VMCDetalle
+    {
+        public int Id_Cupon { get; set; }
+
+        public int Id_ArticuloAsociado { get; set; }
+    }
+}
---
+++ MVCVentas/Models/VMCupon.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCVentas.Models
+{
+    public class VMCupon
+    {
+        public int Id_Cupon { get; set; }
+
+        public decimal PorcentajeDto { get; set; }
+
+        public virtual ICollection<VMCDetalle> Detalle { get; set; }
+    }
+}
